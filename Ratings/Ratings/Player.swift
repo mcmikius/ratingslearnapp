b1c10d9347e59d66e28ff5b8cc741eeb0bb91e17
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-07-10 05:09:58 +0300

    Storyboards Tutorial for iOS: Part 1

---
+++ Ratings/Ratings/Player.swift
@@ -0,0 +1,17 @@
+//
+//  Player.swift
+//  Ratings
+//
+//  Created by Mykhailo Bondarenko on 7/10/19.
+//  Copyright © 2019 Mykhailo Bondarenko. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    
+    // MARK: - Properties
+    var name: String?
+    var game: String?
+    var rating: Int
+}
---
+++ Ratings/Ratings/PlayerCell.swift
@@ -0,0 +1,44 @@
+//
+//  PlayerCell.swift
+//  Ratings
+//
+//  Created by Mykhailo Bondarenko on 7/10/19.
+//  Copyright © 2019 Mykhailo Bondarenko. All rights reserved.
+//
+
+import UIKit
+
+class PlayerCell: UITableViewCell {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var gameLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    
+    // MARK: - Properties
+    var player: Player? {
+        didSet {
+            guard let player = player else { return }
+            
+            gameLabel.text = player.game
+            nameLabel.text = player.name
+            ratingImageView.image = image(forRating: player.rating)
+        }
+    }
+    func image(forRating rating: Int) -> UIImage? {
+        let imageName = "\(rating)Stars"
+        return UIImage(named: imageName)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
